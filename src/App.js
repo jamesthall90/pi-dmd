05a6b42e12f53e6fd6db3d3d0471259bca03b743
James Hall <jamesthall90@gmail.com>
2017-09-21 20:39:27 -0400

    - Created 'Components' directory - Created 'Home' and 'Images' components

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Home from './Component/Home';
+import Images from './Component/Images';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+          <h1>CyberPhysical Systems DVR Project</h1>
+          <ul className="header">
+              <li><IndexLink to="/" activeClassName="active">Home</IndexLink></li>
+              <li><Link to="/images" activeClassName="active">Images</Link></li>
+              <li><Link to="/ScreenShots" activeClassName="active">Screen Shots</Link></li>
+          </ul>
+        <Home/>
+        <Images/>
       </div>
     );
   }
